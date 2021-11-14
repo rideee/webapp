e830d7053c167f254b8b6d128ee1b197a05bf5d8
rideee <michal.katnik91@gmail.com>
2021-11-14 20:58:16 +0100

    Flags added

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -8,9 +9,22 @@ import (
 )
 
 func main() {
-	app := config.NewApp()
-	app.SetPort(8080)
+	// Parse flags.
+	host := flag.String("host", "", "Set application host.")
+	port := flag.Int("port", 8080, "Set application port.")
+	devMode := flag.Bool("devMode", false, "Run application in development mode.")
+	flag.Parse()
 
-	log.Println("Server running on ", app.GetAddress())
+	// Initialize new application config.
+	app := config.NewApp()
+	app.SetHost(*host)
+	app.SetPort(*port)
+	app.SetDevelopmentMode(*devMode)
+
+	// Serve app.
+	if app.InDevMode() {
+		log.Println("Application is running in development mode.")
+	}
+	log.Println("Server is running on", app.GetAddress())
 	log.Fatal(http.ListenAndServe(app.GetAddress(), nil))
 }
