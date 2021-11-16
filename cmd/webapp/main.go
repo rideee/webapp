c3c84ac78b5f3d0ec5974c5b23d9567724560279
rideee <michal.katnik91@gmail.com>
2021-11-16 18:12:10 +0100

    simplified structure

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/rideee/webapp/internal/config"
+	"github.com/rideee/webapp/internal/tmpl"
 )
 
 func main() {
@@ -17,14 +18,20 @@ func main() {
 
 	// Initialize new application config.
 	app := config.NewApp()
-	app.SetHost(*host)
-	app.SetPort(*port)
-	app.SetDevelopmentMode(*devMode)
+	app.Host = *host
+	app.Port = *port
+	app.DevelopmentMode = *devMode
+
+	// TODO: For testing only...
+	t := tmpl.New(app)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t.Render(w, r, "homepage.tmpl")
+	})
 
 	// Serve app.
 	if app.InDevMode() {
 		log.Println("Application is running in development mode.")
 	}
-	log.Println("Server is running on", app.GetAddress())
-	log.Fatal(http.ListenAndServe(app.GetAddress(), nil))
+	log.Println("Server is running on", app.Address())
+	log.Fatal(http.ListenAndServe(app.Address(), nil))
 }
--- internal/config/app.go
+++ internal/config/app.go
@@ -6,51 +6,26 @@ import (
 
 // App holds the application config.
 type App struct {
-	host            string
-	port            int
-	developmentMode bool
-}
-
-// SetHost sets application host.
-func (a *App) SetHost(host string) {
-	a.host = host
-}
-
-// GetHost returns application host.
-func (a *App) GetHost() string {
-	return a.host
-}
-
-// SetPort sets application port.
-func (a *App) SetPort(port int) {
-	a.port = port
-}
-
-// GetPort returns application port.
-func (a *App) GetPort() int {
-	return a.port
+	Host            string
+	Port            int
+	DevelopmentMode bool
 }
 
 // GetAddress returns the host and port in the format host: port (127.0.0.1:8080).
-func (a *App) GetAddress() string {
-	return fmt.Sprintf("%s:%d", a.host, a.port)
-}
-
-// SetDevelopmentMode sets development mode.
-func (a *App) SetDevelopmentMode(mode bool) {
-	a.developmentMode = mode
+func (a *App) Address() string {
+	return fmt.Sprintf("%s:%d", a.Host, a.Port)
 }
 
 // InDevMode() returns true when development mode is active.
 func (a *App) InDevMode() bool {
-	return a.developmentMode
+	return a.DevelopmentMode
 }
 
 // NewApp returns a new App object with default values.
 func NewApp() *App {
 	return &App{
-		host:            "",
-		port:            8080,
-		developmentMode: false,
+		Host:            "",
+		Port:            8080,
+		DevelopmentMode: false,
 	}
 }
